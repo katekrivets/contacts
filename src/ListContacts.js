4b6b5af07add543da796f126c2679d3cd9793645
katerren <katerren@gmail.com>
2018-08-10 18:00:50 +0300

    add elems

---
+++ src/ListContacts.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+
+function ListContacts (props) {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map((contact, index)=> 
+                <li key={contact.id} className='contact-list-item'>
+                    <div className='contact-avatar' style={{ 
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }}/>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.email}</p>
+                    </div>
+                    <button className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            )}
+        </ol>
+    )
+}
+// class ListContacts extends Component {
+//     render () {
+//         return (
+//             <ol className='contact-list'>
+//                 {this.props.contacts.map((contact, index)=> 
+//                     <li key={contact.id} className='contact-list-item'>
+//                         <div className='contact-avatar' style={{ 
+//                             backgroundImage: `url(${contact.avatarURL})`
+//                         }}/>
+//                         <div className='contact-details'>
+//                             <p>{contact.name}</p>
+//                             <p>{contact.email}</p>
+//                         </div>
+//                         <button className='contact-remove'>
+//                             Remove
+//                         </button>
+//                     </li>
+//                 )}
+//             </ol>
+//         )
+//     }
+// }
+
+export default ListContacts
